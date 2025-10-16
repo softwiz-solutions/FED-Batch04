b1d30f9f8687837b9e78e21a8e0622a52ee95c38
Abdul Hanan <abdulhanan3279@gmail.com>
2025-10-16 20:39:41 +0500

    JS Wee 13 Day 1

---
+++ LearnJS/Week 13/Day1/program.js
@@ -0,0 +1,68 @@
+// var name1
+// console.log("name",name1)
+//console.log("Hello world!")
+// console.log("Welcome to JavaScript Class 1")
+// console.log("2+2")
+
+// ******MATH
+// x=10
+// y=20
+// sum=x+y
+
+// var age = 12;
+// console.log("age1", age);
+
+// var age = 19;
+// console.log("age2", age);
+
+// 1. Variable Declaration
+// var variable_name
+// var age , var name , var class
+
+// 2. Variable Initalization
+// age=20 name="ali" class="9th"
+
+// name1="Ali"
+// console.log("name1",name1)
+
+// NAMING RULES:
+// 1. A-Z , a-z , 0-9, _
+// var AGE, age, Class, number1,number10, student_name --> correct
+// var age#name // var age. --> in correct
+
+// 2. Variable name can start with a-z,A-Z,_ , not from 0-9
+// var age var Name var _class --> correct
+// var 0class --> incorrect
+
+// 3. Keyword can't be variable name
+// var return --> incorrect
+// var if --> incorrect
+
+// *************CASES
+// var student_name, snake case
+// var studentName, camel case
+// var StudentName, pascal case
+
+// write a js program, to store the name of user and his age.
+
+var userName = "Shehraz";
+var userAge = 17;
+
+
+console.log("My name is", userName, "my age is", userAge);
+
+
+
+// Data Types
+
+// 1. number
+// var age=19
+// var height=6.5
+// // 2. STring
+// var userName="hello"
+// var demo='A'
+// // 3 . Boolean
+
+// var isHafiz=false
+// var passed=true
+
