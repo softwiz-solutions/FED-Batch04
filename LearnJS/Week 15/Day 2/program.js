35538c6b9ebb46c30b9cb579c2586bd7d9015bc2
Abdul Hanan <abdulhanan3279@gmail.com>
2025-11-01 17:57:14 +0500

    Feat: week 15 day2 loop

---
+++ LearnJS/Week 15/Day 2/program.js
@@ -0,0 +1,72 @@
+// Types of loops in JavaScript
+
+// 1. while loop
+// 2 . do while loop
+// 3. for loop
+
+// Steps
+// 1. initialize a counter variable
+// 2. condition
+// 3. excecution
+// 4. increment/decrement
+
+// 1. while loop
+
+// var counter=0
+// while(conditon){
+// .......
+// counter (increment/decrement)
+// }
+
+// var counter = 1;
+// while (counter <= 5) {
+//   console.log("Pakistan Zindabad!");
+//   counter++;
+// }
+
+// console.log("hello");
+
+// var i = 0;
+// while (i < 10) {
+//   console.log("i=", i);
+//   i++;
+// }
+
+// 2. do while loop
+// 1. initialize a counter variable
+// 2. excecution
+// 3. increment/decrement
+// 4. condition
+
+// var i = 6;
+// do {
+//   console.log("hello");
+//   i++;
+// } while (i < 5);
+
+// var ans = "no";
+// do {
+//   console.log("hello");
+//   ans = prompt("do you want to exit? yes/no");
+// } while (ans === "no");
+
+// console.log("loop ended");
+
+// for(initilization; condition; increment/decrement){
+//     .......
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   console.log("Pakistan Zindabad!", i);
+// }
+// console.log("i", i);
+
+// var ans = "n";
+// while (ans == "n") {
+//   ans = prompt("do you want to exit? y/n");
+// }
+
+
+
+
+
