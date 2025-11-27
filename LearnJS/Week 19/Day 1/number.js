ad9415410e1e30577fa55ccb9d883a51074bd158
Abdul Hanan <abdulhanan3279@gmail.com>
2025-11-27 20:24:20 +0500

    Week 19 day 1

---
+++ LearnJS/Week 19/Day 1/number.js
@@ -0,0 +1,39 @@
+// Round, ciel, floor
+
+// var average=8.9
+// var round=Math.round(average)
+// console.log("🚀 ~ round:", Math.round(average))
+// console.log("🚀 ~ ciel:", Math.ceil(average))
+// console.log("🚀 ~ floor:", Math.floor(average))
+
+// Random
+
+// var randomNumber = Math.round(Math.random() * 1);
+// console.log("🚀 ~ randomNumber:", randomNumber);
+
+// var head = prompt("Enter your first player name");
+// var tail = prompt("Enter your second player name");
+
+// if (randomNumber == 0) {
+//   console.log("tail wins", tail);
+// } else {
+//   console.log("head wins", head);
+// }
+
+// var die = Math.round(Math.random() * 10) + 5;
+// console.log("🚀 ~ die:", die);
+
+
+
+// var num = Math.floor(Math.random() * (max - min + 1)) + min;
+// var num = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+// console.log("🚀 ~ num:", num)
+
+
+
+// var num = Math.floor(Math.random() * (5000 - 3000 + 1)) + 3000;
+// console.log("🚀 ~ num:", num)
+
+
+var number=Number(prompt("Enter number"))
+console.log("🚀 ~ number:", number)
---
+++ LearnJS/Week 19/Day 1/string.js
@@ -0,0 +1,11 @@
+// var text = "Hello world";
+
+// var text = "hello world how are you?";
+// alert("replace anything in the text:" + text);
+// var find = prompt("Enter the text that you want find");
+// var replace = prompt("Enter the text you want to replace");
+
+// text = text.replace(find, replace);
+// console.log("updated text",text)
+// console.log("🚀 ~ newText:", text)
+// console.log("index of w", text.indexOf("s"));
