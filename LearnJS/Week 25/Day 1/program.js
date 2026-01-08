5637e77cebd2e59ea3eebffe447ed1fec4862fe8
Abdul Hanan <abdulhanan3279@gmail.com>
2026-01-08 20:34:56 +0500

    Week 25 Day 1

---
+++ LearnJS/Week 25/Day 1/program.js
@@ -0,0 +1,40 @@
+var student = {
+  name: "John Doe",
+  rollNo: "123",
+  // key:"13",
+};
+
+// console.log("exist",'name' in student)
+// console.log("exist",student.hasOwnProperty('name'))
+
+// var listOfProperties=[]
+// for(var key in student){
+//     // console.log("key",key)
+//     listOfProperties.push(key)
+//     // console.log(student[key])// student['name'] ----> student.'name' X
+// }
+// console.log("list",listOfProperties)
+
+var listOfProperties = Object.keys(student);
+// console.log("list",listOfProperties)
+
+for (var i = 0; i < listOfProperties.length; i++) {
+  // var key=listOfProperties[i]
+  // console.log("key",key)
+  // console.log("value",student[key])
+}
+
+// var listOfEntries=Object.entries(student)
+// console.log("entries",listOfEntries)
+
+console.log("window href", window.location.href);
+console.log("window hostname", window.location.hostname);
+console.log("window pathname    ", window.location.pathname);
+
+var handleLink = function () {
+  window.location.href = "/about.html#contact-section";
+
+  //   var hostname = window.location.hostname;
+  //   var link = "http://" + hostname + "/about.html";
+  //   window.location.href = link;
+};
